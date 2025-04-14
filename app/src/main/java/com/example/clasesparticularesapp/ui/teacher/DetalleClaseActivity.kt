b071b4a5e1e8924fd1d4ea913400b88c03af7d3a
TortillaDeSatan <enriqueanda689@gmail.com>
2025-04-14 13:31:53 +0200

    Vista de la clase con la informacion de la misma

---
+++ app/src/main/java/com/example/clasesparticularesapp/ui/teacher/DetalleClaseActivity.kt
@@ -0,0 +1,31 @@
+package com.example.clasesparticularesapp.ui.teacher
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.clasesparticularesapp.R
+
+
+class DetalleClaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_clase)
+
+        val nombre = intent.getStringExtra("nombre")
+        val asignatura = intent.getStringExtra("asignatura")
+        val descripcion = intent.getStringExtra("descripcion")
+        val fecha = intent.getStringExtra("fecha")
+        val horario = intent.getStringExtra("horario")
+        val limite = intent.getIntExtra("limiteAlumnos", 0)
+        val precio = intent.getFloatExtra("precioHora", 0f)
+
+        findViewById<TextView>(R.id.tvNombre).text = "Clase: $nombre"
+        findViewById<TextView>(R.id.tvAsignatura).text = "Asignatura: $asignatura"
+        findViewById<TextView>(R.id.tvDescripcion).text = "Descripción: $descripcion"
+        findViewById<TextView>(R.id.tvFecha).text = "Fecha: $fecha"
+        findViewById<TextView>(R.id.tvHorario).text = "Horario: $horario"
+        findViewById<TextView>(R.id.tvLimite).text = "Límite de alumnos: $limite"
+        findViewById<TextView>(R.id.tvPrecio).text = "Precio por hora: $precio €"
+    }
+}
